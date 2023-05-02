02198d2bf78e4eb057731f40b70427e99ddc2e6f
subhanje <subhanjee14@gmail.com>
2023-05-03 01:17:13 +0500

    done

---
+++ components/NewsTwo/NewsTwoRightSection.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Styles from "../../styles/NewsTwoRightSection.module.css";
+import Image from "next/image";
+import Bottle from "../../public/images/Rectangle 45.png";
+import Shoes from "../../public/images/Rectangle 46 (1).png";
+import Heart from "../../public/images/Frame 44373.png";
+import Forward from "../../public/images/Frame 44375.png";
+import Comments from "../../public/images/Frame 44373 (1).png";
+import Ellipse from "../../public/images/Ellipse 207.png";
+import { Input, Select } from "antd";
+import FeaturedNews from "../News/FeaturedNews";
+import Green from "../../public/images/Rectangle 4636.png";
+import NewsPost from "./NewsPost";
+const { Option } = Select;
+function NewsTwoRightSection() {
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+  return (
+    <div>
+      <div className={Styles.boxflex}>
+        <Image className={Styles.imgsize} src={Bottle} alt="abc" />
+        <Image className={Styles.imgsize} src={Shoes} alt="abc" />
+      </div>
+      <div className={Styles.boxgreen}>
+        <div className={Styles.greenbox}>
+          <p> Breaking News</p>
+        </div>
+        <div>
+          <p className={Styles.parh}>
+            Lorem Ipsum is simply dummy text of the printing and types Lorem
+            Ipsum is simply dummy text of the printing
+          </p>
+        </div>
+      </div>
+      <div>
+        <button className={Styles.btnnew}>News</button>
+        <p className={Styles.howtext}>
+          How to write a title with an optimal width for SEO
+        </p>
+      </div>
+      <div className={Styles.mainimgelldiv}>
+        <div className={Styles.elldiv}>
+          <Image src={Ellipse} />
+          <div className={Styles.textdiv}>
+            <p style={{ color: "#444444", fontWeight: "500" }}>Author Name</p>
+            <p
+              style={{ color: "#A1A1A1", fontWeight: "500", fontSize: ".7rem" }}
+            >
+              02 Jan 2023
+            </p>
+          </div>
+        </div>
+        <div className={Styles.commdiv}>
+          <Image className={Styles.smllbox} src={Heart} />
+          <Image className={Styles.smllbox} src={Forward} />
+          <Image className={Styles.smllbox} src={Comments} />
+          <p>50k</p>
+        </div>
+      </div>
+      <div>
+        <NewsPost />
+      </div>
+    </div>
+  );
+}
+
+export default NewsTwoRightSection;
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,19 @@ import HeroSection from "@/components/HeroSection";
 // import LandingPage from "./landingPage";
 // import TopicPage from "./TopicPage";
 // import MembersPage from "./membersPage";
-import MembersPageTwo from "./memberPageTwo";
-import MainHeader from "@/components/common/mainHeader";
-import Footer from "@/components/common/footer";
-import LandingPage from "./landingPage";
-import NewsPage from "./NewsPage";
+// import MembersPageTwo from "./memberPageTwo";
+// import MainHeader from "@/components/common/mainHeader";
+// import Footer from "@/components/common/footer";
+// import LandingPage from "./landingPage";
+// import NewsPage from "./NewsPage";
+import NewsPageTwo from "./NewsPageTwo";
 export default function Home() {
   return (
     <>
       {/* <MainHeader /> */}
       {/* <LandingPage /> */}
       {/* <Footer /> */}
-      <NewsPage />
+      <NewsPageTwo />
     </>
   );
 }
