770063f218cbc7cc902322b96abf6a97de06e5e2
usamaahma <usamaahmad087@gmail.com>
2023-05-05 14:04:19 +0500

    final screens

--- components/NewsTwo/NewsPost.js
+++ components/NewsTwo/NewsPost.js
@@ -16,18 +16,11 @@ function NewsPost() {
       <div>
         <Image className={Styles.imgstyle} src={Business} alt="abc" />
         <div className={Styles.boxpadding}>
-          <p className={Styles.lookingtext}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy
-            textever since the 1500s, Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry. Lorem Ipsum has been the
-            industry'sstandard dummy textever since the 1500s, <br />
-            <br /> Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's standard
-            dummy textever since the 1500s, Lorem Ipsum is simply dummy text of
-            the printing and typesetting industry. Lorem Ipsum has been the
-            industry'sstandard dummy textever since the 1500s,
-          </p>
+        <p className={Styles.lookingtext}>
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, <br />
+  <br /> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
+</p>
+
         </div>
       </div>
       <div className={Styles.mblview}>
