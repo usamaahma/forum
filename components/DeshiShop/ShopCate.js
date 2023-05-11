0c81cf21e78f1a3218b2a4297d5e6ae41ea4bcc3
talalnuman1 <developertrisage1@gmail.com>
2023-05-11 15:55:43 +0500

    done34

--- components/DeshiShop/ShopCate.js
+++ components/DeshiShop/ShopCate.js
@@ -1,23 +1,93 @@
 import React from "react";
 import Styles from "../../styles/ShopCate.module.css";
 import ProductCard from "./ProductCard";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <div
+    key={1}
+    onDragStart={handleDragStart}
+    style={{ display: "flex", justifyContent: "center" }}
+  >
+    <ProductCard />
+  </div>,
+  <div
+    key={2}
+    onDragStart={handleDragStart}
+    style={{ display: "flex", justifyContent: "center" }}
+  >
+    <ProductCard />
+  </div>,
+  <div
+    key={3}
+    onDragStart={handleDragStart}
+    style={{ display: "flex", justifyContent: "center" }}
+  >
+    <ProductCard />
+  </div>,
+  <div
+    key={4}
+    onDragStart={handleDragStart}
+    style={{ display: "flex", justifyContent: "center" }}
+  >
+    <ProductCard />
+  </div>,
+  <div
+    key={5}
+    onDragStart={handleDragStart}
+    style={{ display: "flex", justifyContent: "center" }}
+  >
+    <ProductCard />
+  </div>,
+  <div
+    key={6}
+    onDragStart={handleDragStart}
+    style={{ display: "flex", justifyContent: "center" }}
+  >
+    <ProductCard />
+  </div>,
+  <div
+    key={7}
+    onDragStart={handleDragStart}
+    style={{ display: "flex", justifyContent: "center" }}
+  >
+    <ProductCard />
+  </div>,
+];
+
+const responsive = {
+  0: { items: 1 },
+  568: { items: 2 },
+  1024: { items: 4 },
+  1440: { items: 5 },
+  2560: { items: 5 },
+};
 function ShopCate() {
   return (
     <div>
-      <div>
-        <h3>Shop by Category</h3>
-        <div
-          style={{
-            backgroundColor: "#42b00f",
-            borderWidth: 1,
-            width: "9rem",
-            border: "#42b00f 1px solid",
-            position: "absolute",
-          }}
-        ></div>
-        <hr />
+      <div className={Styles.centercaro}>
+        <div className={Styles.textdiv}>
+          <h3>Popular Products</h3>
+          <p className={Styles.viewtext}>View all</p>
+          <div className={Styles.greenline}></div>
+        </div>
+        <hr className={Styles.hrline} />
         <div>
-          <ProductCard />
+          <div className={Styles.carodiv}>
+            <AliceCarousel
+              mouseTracking
+              items={items}
+              responsive={responsive}
+              // disableDotsControls
+              disableButtonsControls
+              autoPlay
+              autoPlayInterval={2000}
+              disableAutoPlayOnAction={false}
+            />
+          </div>
         </div>
       </div>
     </div>
