361723e3688584efae2d9b69c29ed6fce653edc2
talalnuman1 <developertrisage1@gmail.com>
2023-05-11 14:09:16 +0500

    deshi

--- components/DeshiShop/LeftSectionOfDeshi.js
+++ components/DeshiShop/LeftSectionOfDeshi.js
@@ -1,18 +1,72 @@
 import React from "react";
 import Styles from "../../styles/LeftSectionOfDeshi.module.css";
-import Right from "../../public/images/Right-arrow.png";
 import Image from "next/image";
 import { RightOutlined } from "@ant-design/icons";
+import { Dropdown } from "antd";
+const items = [
+  {
+    key: "1",
+    label: <a> CHOCOLATE & CHIPS</a>,
+  },
+  {
+    key: "2",
+    label: <a>COOKING & BAKING NEEDS</a>,
+  },
+  {
+    key: "3",
+    label: <a>DAIRY</a>,
+  },
+  {
+    key: "4",
+    label: <a>FRUITS & VEGETABLES</a>,
+  },
+  {
+    key: "5",
+    label: <a>GROCERY ITEMS</a>,
+  },
+  {
+    key: "6",
+    label: <a>HOME MADE FOOD</a>,
+  },
+  {
+    key: "7",
+    label: <a>MEAT & FISH</a>,
+  },
+  {
+    key: "8",
+    label: <a>PICKLE, SPICE & SAUCE</a>,
+  },
+  {
+    key: "9",
+    label: <a>SNACKS ITEMS</a>,
+  },
+  {
+    key: "10",
+    label: <a>TEA, BEVERAGE & DRINKS</a>,
+  },
+];
 function LeftSectionOfDeshi() {
   return (
     <div>
       <div className={Styles.greybox}>
-        <div className={Styles.greysmll}>
+        <div>
           <h3>Shops</h3>
         </div>
-        <div className={Styles.greysmll}>
-          <p className={Styles.text}>Consumer Food</p>
-          <RightOutlined className={Styles.text1} />
+        <div>
+          <Dropdown
+            menu={{
+              items,
+            }}
+            placement="bottomLeft"
+            arrow={{
+              pointAtCenter: true,
+            }}
+          >
+            <div className={Styles.greysmll}>
+              <p className={Styles.text}>Consumer Food</p>
+              <RightOutlined className={Styles.text1} />
+            </div>
+          </Dropdown>
         </div>
         <hr style={{ border: "1px solid #E8E8E8" }} />
         <div className={Styles.greysmll}>
@@ -37,6 +91,7 @@ function LeftSectionOfDeshi() {
       </div>
     </div>
   );
+  v;
 }
 
 export default LeftSectionOfDeshi;
