f8e84e38169ac35d668c44efe25c92ab80c316a8
usamaahma <usamaahmad087@gmail.com>
2023-05-18 16:18:53 +0500

    new code pushed

---
+++ pages/DeshiShopCheckout.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Styles from "../styles/DeshiShopPageTwo.module.css";
+import Footer from "@/components/common/footer";
+import MainHeader from "@/components/common/mainHeader";
+import { Row, Col } from "antd";
+
+import LeftSectionCheckout from "@/components/DeshiCheckout/LeftSectionCheckout";
+import RightSectioncheckout from "@/components/DeshiCheckout/RightSectioncheckout";
+
+function DeshiShopCheckout() {
+  return (
+    <div>
+      <MainHeader />
+      <div>
+        <Row justify="center">
+          <Col>
+            <div className={Styles.centerCol}>
+              <LeftSectionCheckout />
+            </div>
+          </Col>
+          <Col>
+            <div className={Styles.centerCol12}>
+              {" "}
+              <RightSectioncheckout />
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default DeshiShopCheckout;
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,12 @@ import DeshiShopPageTwo from "./DeshiShopPageTwo";
 import DeshiShopPage from "./DeshiShopPage";
 import LandingPage from "./landingPage";
 import DeshiStoreTwo from "./DeshiStoreTwo";
+import DeshiShopCheckout from "./DeshiShopCheckout";
 
 export default function Home() {
   return (
     <>
-      <DeshiStoreTwo />
+      <DeshiShopCheckout />
     </>
   );
 }
