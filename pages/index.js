d49fda296cc3c656a99f46a7bf826ed50780fc06
usamaahma <usamaahmad087@gmail.com>
2023-05-18 12:05:09 +0500

    new code

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import DeshiShopStore from "./DeshiShopStore";
 import DeshiShopPageTwo from "./DeshiShopPageTwo";
 import DeshiShopPage from "./DeshiShopPage";
 import LandingPage from "./landingPage";
+import DeshiStoreTwo from "./DeshiStoreTwo";
 
 export default function Home() {
   return (
     <>
-      <DeshiShopStore />
+      <DeshiStoreTwo />
     </>
   );
 }
