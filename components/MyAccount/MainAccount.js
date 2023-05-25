78e7c1b8c87e5b7047d017aa86564d8acefdb046
subhanje <subhanjee14@gmail.com>
2023-05-25 13:17:41 +0500

    done

---
+++ components/MyAccount/MainAccount.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Styles from "../../styles/MainAccount.module.css";
+import { Col, Row } from "antd";
+function MainAccount() {
+  return (
+    <div>
+      <div>
+        <Row>
+          <Col>
+            <div></div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+}
+
+export default MainAccount;
