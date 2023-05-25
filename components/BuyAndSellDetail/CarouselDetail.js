a35194d71dabc6e1ec98b9d2e21eedc56abe6ba0
subhanje <subhanjee14@gmail.com>
2023-05-25 12:13:05 +0500

    done

--- components/BuyAndSellDetail/CarouselDetail.js
+++ components/BuyAndSellDetail/CarouselDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Styles from "../../styles/CarouselDeshi.module.css";
+import Styles from "../../styles/CarouselDetail.module.css";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import detail from "../../detail.json";
