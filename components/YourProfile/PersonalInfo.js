6c8d4d4471b72c03ce03d95884b8136f2fee84a4
subhanje <subhanjee14@gmail.com>
2023-05-27 12:05:04 +0500

    der

--- components/YourProfile/PersonalInfo.js
+++ components/YourProfile/PersonalInfo.js
@@ -1,8 +1,51 @@
 import React from "react";
 import Styles from "../../styles/PersonalInfo.module.css";
 import Edit from "../../public/images/Edit.png";
+import Pic from "../../public/images/pic.png";
 import Image from "next/image";
-import { Col, Row, Button, Checkbox, Form, Input } from "antd";
+import { Col, Row, Select, Form, Input, Button, message, Upload } from "antd";
+import { CloudDownloadOutlined, CameraOutlined } from "@ant-design/icons";
+
+const props = {
+  name: "file",
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+const { Dragger } = Upload;
+const props1 = {
+  name: "file",
+  multiple: true,
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log("Dropped files", e.dataTransfer.files);
+  },
+};
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
 const onFinish = (values) => {
   console.log("Success:", values);
 };
@@ -387,10 +430,20 @@ function PersonalInfo() {
                       ]}
                     >
                       <p className={Styles.namtext}>Upload Photo</p>
-
-                      <Input
-                        className={Styles.input}
-                        placeholder="09/02/2023"
+                      <Select
+                        defaultValue="Yes"
+                        onChange={handleChange}
+                        allowClear
+                        options={[
+                          {
+                            value: "Yes",
+                            label: "Yes",
+                          },
+                          {
+                            value: "No",
+                            label: "No",
+                          },
+                        ]}
                       />
                     </Form.Item>
                   </Form>
@@ -455,6 +508,40 @@ function PersonalInfo() {
               </div>
             </Col>
           </Row>
+          <div className={Styles.updiv}>
+            <div className={Styles.centerup}>
+              <Image src={Pic} alt="" />
+              <Upload {...props}>
+                <Button className={Styles.btn} icon={<CloudDownloadOutlined />}>
+                  Upload Photo
+                </Button>
+              </Upload>
+              <Upload {...props}>
+                <Button className={Styles.btn} icon={<CameraOutlined />}>
+                  Take Photo
+                </Button>
+              </Upload>
+            </div>
+            <div className={Styles.divcneter}>
+              <Dragger {...props1}>
+                <div className={Styles.drag}>
+                  <p className={Styles.grenclr}>
+                    <CloudDownloadOutlined />
+                  </p>
+                  <p className={Styles.textupp}>
+                    Upload Cover Photo <br />
+                    or Drag & Drop
+                  </p>
+                  <p className={Styles.jptext}>
+                    JPG, PDF, file size no more than 3 MB 270 x 158 recommended
+                  </p>
+                </div>
+              </Dragger>
+            </div>
+          </div>
+          <div>
+            <button className={Styles.greenbtn}>Save</button>
+          </div>
         </div>
       </div>
     </div>
