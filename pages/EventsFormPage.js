9b2dcea7c74dc192da6b1dd1d74c217f712bb02f
subhanje <subhanjee14@gmail.com>
2023-08-15 15:45:05 +0500

    first commit

--- pages/EventsFormPage.js
+++ pages/EventsFormPage.js
@@ -3,8 +3,6 @@ import Styles from "../styles/Topic.module.css";
 import { Row, Col } from "antd";
 import MainHeader from "@/components/common/mainHeader";
 import Footer from "@/components/common/footer";
-import EventLeftSection from "@/components/eventPageTwo/EventLeftSection";
-import EventRightSection from "@/components/eventPageTwo/EventRightSection";
 import EventHeroSection from "@/components/eventPageTwo/EventHeroSection";
 import Eventform from "@/components/eventpage/eventform";
 
