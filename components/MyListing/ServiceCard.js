288b544a265482e087955de557b54aba61ce3315
subhanje <subhanjee14@gmail.com>
2023-10-23 20:01:03 +0500

    done

--- components/MyListing/ServiceCard.js
+++ components/MyListing/ServiceCard.js
@@ -1,77 +1,112 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Col, Row } from "antd";
 import Styles from "../../styles/servicecard.module.css";
 import Switch from "./Switch";
-
-const data = [
-  {
-    key: "1",
-    image: (
-      <img
-        className={Styles.imgmaincard}
-        alt="abc"
-        src="../images/servicecard.png"
-      />
-    ),
-    company: "Construction",
-    price: "600$",
-    name: "Siam Accounting LLC,Jackson Trading ....",
-    detail: "Design Communication Studio",
-    status: "pending",
-    time: "1 Hour",
-    place: "New York",
-    views: "3",
-  },
-  {
-    key: "2",
-    image: (
-      <img
-        className={Styles.imgmaincard}
-        alt="abc"
-        src="../images/servicecard.png"
-      />
-    ),
-    company: "Construction",
-    price: "600$",
-    name: "Siam Accounting LLC,Jackson Trading ....",
-    detail: "Design Communication Studio",
-    status: "pending",
-    time: "1 Hour",
-    place: "New York",
-    views: "3",
-  },
-  {
-    key: "3",
-    image: (
-      <img
-        className={Styles.imgmaincard}
-        alt="abc"
-        src="../images/servicecard.png"
-      />
-    ),
-    company: "Construction",
-    price: "600$",
-    name: "Siam Accounting LLC,Jackson Trading ....",
-    detail: "Design Communication Studio",
-    status: "pending",
-    time: "1 Hour",
-    place: "New York",
-    views: "3",
-  },
-];
+import { deshiOneForm } from "@/helper/axios";
 
 function ServiceCard() {
+  const [hovered, setHovered] = useState(false);
+  // const router = useRouter();
+  const [data, setData] = useState();
+  const [loading, setloading] = useState();
+  // const data = [
+  //   {
+  //     key: "1",
+  //     image: (
+  //       <img
+  //         className={Styles.imgmaincard}
+  //         alt="abc"
+  //         src="../images/servicecard.png"
+  //       />
+  //     ),
+  //     company: "Construction",
+  //     price: "600$",
+  //     name: "Siam Accounting LLC,Jackson Trading ....",
+  //     detail: "Design Communication Studio",
+  //     status: "pending",
+  //     time: "1 Hour",
+  //     place: "New York",
+  //     views: "3",
+  //   },
+  //   {
+  //     key: "2",
+  //     image: (
+  //       <img
+  //         className={Styles.imgmaincard}
+  //         alt="abc"
+  //         src="../images/servicecard.png"
+  //       />
+  //     ),
+  //     company: "Construction",
+  //     price: "600$",
+  //     name: "Siam Accounting LLC,Jackson Trading ....",
+  //     detail: "Design Communication Studio",
+  //     status: "pending",
+  //     time: "1 Hour",
+  //     place: "New York",
+  //     views: "3",
+  //   },
+  //   {
+  //     key: "3",
+  //     image: (
+  //       <img
+  //         className={Styles.imgmaincard}
+  //         alt="abc"
+  //         src="../images/servicecard.png"
+  //       />
+  //     ),
+  //     company: "Construction",
+  //     price: "600$",
+  //     name: "Siam Accounting LLC,Jackson Trading ....",
+  //     detail: "Design Communication Studio",
+  //     status: "pending",
+  //     time: "1 Hour",
+  //     place: "New York",
+  //     views: "3",
+  //   },
+  // ];
+  const getDeshiOneForm = () => {
+    setloading(true);
+    // let token = localStorage.getItem("talbeilm-token");
+
+    deshiOneForm({
+      method: "get",
+      headers: {
+        // Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res.data.results, "user");
+        setData(res.data.results);
+        setloading(false);
+      })
+      .catch(() => {
+        message.error("an error occured please try later");
+        setloading(false);
+      });
+  };
+  useEffect(() => {
+    getDeshiOneForm();
+  }, []);
   return (
     <div>
       <div>
-        {data.map((data, index) => (
+        {data?.map((item, index) => (
           <div key={index}>
             <Card bordered={false}>
               <Row className={Styles.gapro}>
-                <Col>{data.image}</Col>
+                <Col>
+                  {" "}
+                  <img
+                    src={item.image?.[0]}
+                    alt="abc"
+                    width={150}
+                    height={150}
+                  />
+                </Col>
                 <Col>
                   <div className={Styles.butico}>
-                    <button className={Styles.btnconst}>{data.company}</button>
+                    <button className={Styles.btnconst}>Construction</button>
                     <div className={Styles.gapro}>
                       <img
                         className={Styles.imgs}
@@ -86,13 +121,13 @@ function ServiceCard() {
                     </div>
                   </div>
                   <div className={Styles.seconddiv}>
-                    <p className={Styles.nam}>{data.name}</p>
-                    <p className={Styles.pric}>{data.price}</p>
+                    <p className={Styles.nam}>{item.title}</p>
+                    <p className={Styles.pric}>{item.price}</p>
                   </div>
-                  <p className={Styles.det}>{data.detail}</p>
+                  <p className={Styles.det}>{item.metaDescription}</p>
                   <div className={Styles.stat}>
                     <p>Status:</p>
-                    <button className={Styles.btncomp}>{data.status}</button>
+                    <button className={Styles.btncomp}>pending</button>
                   </div>
                   <Row className={Styles.rowlast}>
                     <Col className={Styles.collstw}>
@@ -111,7 +146,7 @@ function ServiceCard() {
                           alt="abc"
                           src="../images/locationone.png"
                         />{" "}
-                        <p>{data.place}</p>{" "}
+                        <p>{item.address}</p>{" "}
                       </div>
                       <div className={Styles.collstww}>
                         <img
